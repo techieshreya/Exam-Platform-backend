2a1ea26cf52e4e206cd1b721ecfcebf17950f0a1
techieshreya <shreyagupta.ggn@gmail.com>
2025-04-08 20:06:11 +0530

    fix: update drizzle-orm and pg dependencies to latest versions; adjust import for drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,4 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/node-postgres";
+
 export const db = drizzle(process.env.DATABASE_URL!);
 // const result = await db.execute("select 1");
